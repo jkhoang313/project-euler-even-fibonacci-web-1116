5a06038c11e81cbaf40324b2dfd73c475cc34f2e
franco83-2 <franco.84@gmail.com>
2017-01-04 10:10:09 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,1 +1,7 @@
-# Implement your procedural solution here!
+def even_fibonacci_sum(limit)
+ a = [1,2]
+ while a[-2] + a[-1] < limit
+   a << a[-2] + a[-1]
+ end
+ a.select {|n| n.even? }.inject(:+)
+end
--- lib/oo_even_fibonacci.rb
+++ lib/oo_even_fibonacci.rb
@@ -1,1 +1,15 @@
-# Implement your object-oriented solution here!
+class EvenFibonacci
+
+  def initialize(limit)
+    @limit = limit
+  end
+
+  def sum
+   a = [1,2]
+   while a[-2] + a[-1] < @limit
+     a << a[-2] + a[-1]
+   end
+   a.select {|n| n.even? }.inject(:+)
+  end
+
+end
