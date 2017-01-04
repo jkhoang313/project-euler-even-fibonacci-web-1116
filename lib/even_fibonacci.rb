e85ac12536084cda091405a2ffe488d2ed65adca
franco83-2 <franco.84@gmail.com>
2017-01-04 10:11:43 -0500

    Done.

--- lib/even_fibonacci.rb
+++ lib/even_fibonacci.rb
@@ -1,7 +1,7 @@
 def even_fibonacci_sum(limit)
  a = [1,2]
- while a[-2] + a[-1] < limit
-   a << a[-2] + a[-1]
- end
+   while a[-2] + a[-1] < limit
+     a << a[-2] + a[-1]
+   end
  a.select {|n| n.even? }.inject(:+)
 end
